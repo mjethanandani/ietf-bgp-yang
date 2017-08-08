b3587e44896b80d229d89eb8721e2a2ff9b23546
Mahesh Jethanandani <mjethanandani@gmail.com>
2017-08-07 18:36:51 -0700

    Models compile now.

--- src/gen-trees.sh
+++ src/gen-trees.sh
@@ -1,8 +1,10 @@
-for i in yang/ietf-*\@$(date +%Y-%m-%d).yang
+#!/bin/sh
+for i in ../build/ietf-*\@$(date +%Y-%m-%d).yang
 do
-	name=$(echo $i | cut -f 1 -d '.')
-	echo $name
-	pyang -p ../ -f tree $name.yang > $name-tree.txt.tmp
-	fold -w 71 $name-tree.txt.tmp > $name-tree.txt
+	filename=$(basename "$i")
+        name="${filename%.*}"
+        echo $name
+	pyang -p ../build -f tree ../build/$name.yang > ../build/$name-tree.txt.tmp
+	fold -w 71 ../build/$name-tree.txt.tmp > ../build/$name-tree.txt
 done
-rm yang/*-tree.txt.tmp
+rm ../build/*-tree.txt.tmp
