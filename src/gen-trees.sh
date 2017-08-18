1fef1fb71e1ba99d8786a1378d5058b3f8addc06
Mahesh Jethanandani <mjethanandani@gmail.com>
2017-08-17 17:30:57 -0700

    Added build directory and fixed draft to include models from src dir.

--- src/gen-trees.sh
+++ src/gen-trees.sh
@@ -4,7 +4,7 @@ do
 	filename=$(basename "$i")
         name="${filename%.*}"
         echo $name
-	pyang -p ../build -f tree ../build/$name.yang > ../build/$name-tree.txt.tmp
+	pyang --ietf -p ../build -f tree ../build/$name.yang > ../build/$name-tree.txt.tmp
 	fold -w 71 ../build/$name-tree.txt.tmp > ../build/$name-tree.txt
 done
 rm ../build/*-tree.txt.tmp
