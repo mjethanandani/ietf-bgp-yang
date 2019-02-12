8e8161aac1cc0055ec385e2a2cc4c31f7e6324f9
Mahesh Jethanandani <mjethanandani@gmail.com>
2019-02-12 15:57:25 -0800

    Version -04

--- src/validate-and-gen-trees.sh
+++ src/validate-and-gen-trees.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-for i in yang/*\@$(date +%Y-%m-%d).yang
+for i in ../bin/*\@$(date +%Y-%m-%d).yang
 do
-    name=$(echo $i | cut -f 1 -d '.')
+    name=$(echo $i | cut -f 1-3 -d '.')
     echo "Validating $name.yang"
     if test "${name#^example}" = "$name"; then
-        response=`pyang --lint --strict --canonical -p ../../ -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
+        response=`pyang --lint --strict --canonical -p ../../iana/yang-parameters -p ../bin/submodules -p ../bin -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
     else            
-        response=`pyang --ietf --strict --canonical -p ../../ -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
+        response=`pyang --ietf --strict --canonical -p ../../iana/yang-parameters -p ../bin/submodules -p ../bin -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
     fi
     if [ $? -ne 0 ]; then
         printf "$name.yang failed pyang validation\n"
@@ -17,7 +17,7 @@ do
         exit 1
     fi
     fold -w 71 $name-tree.txt.tmp > $name-tree.txt
-    response=`yanglint -p ../../ $name.yang`
+    response=`yanglint -p ../../iana/yang-parameters -p ../src/yang/submodules -p ../src/yang $name.yang`
     if [ $? -ne 0 ]; then
         printf "$name.yang failed yanglint validation\n"
         printf "$response\n\n"
@@ -25,7 +25,7 @@ do
         exit 1
     fi
 done
-rm yang/*-tree.txt.tmp
+rm ../bin/*-tree.txt.tmp
 
 #echo "Validating examples"
 
